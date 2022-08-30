a4dd1de48e1d549743d6c81bb240a6e6dc676be3
Prajwal100 <iar.prajwal@gmail.com>
2022-08-30 22:38:07 +0545

    update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const APP_ENV = process.env.NODE_ENV || 'development'
 export const API_URL = process.env.REACT_APP_URL || 'http://localhost:5000'
 
-export const UPLOAD_URL = process.env.UPDATE_URL || 'http://localhost:5000/api'
+export const UPLOAD_URL = process.env.UPLOAD_URL || 'http://localhost:5000/api'
