8972b59a9865fc182fd3aca5618016027cf05062
Prajwal100 <iar.prajwal@gmail.com>
2022-09-28 14:16:16 +0545

    update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,13 @@
 export const APP_ENV = 'development'
-export const API_URL = process.env.REACT_APP_URL || 'https://kosheli-api.herokuapp.com'
 
-export const UPLOAD_URL =
-  process.env.REACT_APP_UPLOAD_URL || 'https://kosheli-api.herokuapp.com/api'
+//============================  for local =======================
+
+// export const API_URL = process.env.REACT_APP_URL || 'https://kosheli-api.herokuapp.com'
+
+// export const UPLOAD_URL =
+//   process.env.REACT_APP_UPLOAD_URL || 'https://kosheli-api.herokuapp.com/api'
+
+//=================== for production===================
+export const API_URL = 'https://kosheli-api.herokuapp.com'
+
+export const UPLOAD_URL = 'https://kosheli-api.herokuapp.com/api'
