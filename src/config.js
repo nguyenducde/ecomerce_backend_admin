f5f639f72fdce441c532d7921b6aa91f280d6c9a
nguyenducde <ducde678@gmail.com>
2023-09-22 15:16:32 +0700

    api

--- src/config.js
+++ src/config.js
@@ -8,6 +8,6 @@ export const APP_ENV = 'development'
 //   process.env.REACT_APP_UPLOAD_URL || 'https://kosheli-api.herokuapp.com/api'
 
 //=================== for production===================
-export const API_URL = 'https://ecomerce-api-vrfd.onrender.com'
+export const API_URL = 'https://effervescent-crisp-c01dc0.netlify.app'
 
-export const UPLOAD_URL = 'https://ecomerce-api-vrfd.onrender.com/api'
+export const UPLOAD_URL = 'https://effervescent-crisp-c01dc0.netlify.app/api'
